3038df2c9bee62633841ffe66aeea37767bf8c4f
LucieChev <lchevanche@hotmail.fr>
2023-11-06 10:51:08 +0100

    installation front

---
+++ Front/src/components/RetroLink.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Retrospective</h1>
+    </div>
+  </template>
+  
+<script setup lang="ts">
+</script>
+
+
+<style scoped>
+</style>
---
+++ Front/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import RetroLink from '../components/RetroLink.vue'
+</script>
+
+<template>
+  <main>
+    <RetroLink />
+  </main>
+</template>
